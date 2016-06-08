18f83197dadd1db5ebd455c406355a856310a75b
Madhulika Mukherjee <madhulikamukherjee.15@gmail.com>
2016-06-08 12:21:35 +0530

    Added Sterling data

--- jupiter/sentient/custom_task_sterling.py
+++ jupiter/sentient/custom_task_sterling.py
@@ -61,4 +61,4 @@ def minitask():
 		obj2.save()
 if __name__ == '__main__':
 	# main()
-	CustomTask(file_name,"8NxZgAVK8eD5MBQjZda",2).run_csv()
+	CustomTask(file_name,"7jl9Qq3y8Nq4LLNYlGy",2).run_csv()
