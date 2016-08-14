0edce68cfa0ccde7277dc3222cedf51c00d5ffc8
Madhulika Mukherjee <madhulikamukherjee.15@gmail.com>
2016-08-14 22:58:40 +0530

    initiating sterling

--- jupiter/sentient/custom_task_booking.py
+++ jupiter/sentient/custom_task_booking.py
@@ -3,10 +3,10 @@ Read from csv file .
 add to jupiter
 """
 import csv
-from jupiter.sentient.model import TripAdvisorQ
+from jupiter.sentient.model import BookingQ
 from mongoengine import *
 
-file_name="jupiter/sentient/custom_data/sterling/trip.csv"
+file_name="jupiter/sentient/custom_data/sterling/booking-local.csv"
 class Relation(Document):
     """docstring for Relation"""
     survey_id = StringField()
@@ -20,7 +20,7 @@ class ClientAspects(Document):
 class ClientProviders(Document):
 	parent_id = StringField(required = True)
 	providers = ListField()
-   
+
 class CustomTask(object):
 	"""docstring for CustomTask"""
 	def __init__(self,file_name,custom_survey_id,buffer_n=None,mode="csv"):
@@ -28,7 +28,7 @@ class CustomTask(object):
 		self.c= custom_survey_id
 		self.b= buffer_n
 		self.aspect_list = ["ambience","value_for_money","room_service","cleanliness","amenities"]
-	
+
 	def run_csv(self):
 		with open(self.f,"rt") as f:
 			reader= list(csv.reader(f))
@@ -49,14 +49,14 @@ class CustomTask(object):
 	def add_task(self,i):
 		try:
 			survey_id= i[1]
-			obj2=TripAdvisorQ()
-			obj2.base_url=i[2]
-			obj2.survey_id=survey_id
-			obj2.parent_id=self.c
-			obj2.time_review="1980-01-01"
-			obj2.unique_identifier=survey_id+"tripadvisor"
-			obj2.aspect_notation=self.aspect_list
-			obj2.save()
+			obj3=BookingQ()
+			obj3.base_url=i[2]
+			obj3.survey_id=survey_id
+			obj3.parent_id=self.c
+			obj3.time_review="1980-01-01"
+			obj3.unique_identifier=survey_id+"booking"
+			obj3.aspect_notation=self.aspect_list
+			obj3.save()
 			print(survey_id,"has been added")
 		except Exception as e:
 			print("Following exception has happened: ",e)
@@ -69,24 +69,24 @@ class CustomTask(object):
 		if len(objects) == 0:
 			obj2 = Relation()
 			obj2.survey_id=survey_id
-			obj2.provider=["tripadvisor"]
+			obj2.provider=["booking"]
 			obj2.parent=self.c
 			obj2.save()
 			print ("added new relation object")
 		else:
-			newObj = Relation()
-			newObj = objects[0]
-			newObj.provider.append("tripadvisor")
-			newObj.save()
-			print ("updated old relation object")
+			if "booking" not in objects[0].provider:
+				newObj = Relation()
+				newObj = objects[0]
+				newObj.provider.append("booking")
+				newObj.save()
+				print ("updated old relation object with provider")
+			else:
+				print ("provider was already there in old relation object")
 
 	def add_aspects(self, parent_id):
-
 		print ("ADDING ASPECTS")
 		objects = ClientAspects.objects(parent_id = parent_id)
-
 		print ("Number of objects for ", parent_id, "are", len(objects))
-
 		if len(objects) == 0:
 			obj = ClientAspects()
 			obj.parent_id = parent_id
@@ -94,29 +94,22 @@ class CustomTask(object):
 			obj.save()
 
 	def add_providers(self, parent_id):
-
 		print ("ADDING PROVIDERS")
 		objects = ClientProviders.objects(parent_id = parent_id)
 		print ("Number of objects for ", parent_id, "are", len(objects))
 		if len(objects) == 0:
 			obj = ClientProviders()
 			obj.parent_id = parent_id
-			obj.providers = ["tripadvisor"]
+			obj.providers = ["booking"]
 			obj.save()
 		else:
-			if "tripadvisor" not in objects[0].providers:
+			if "booking" not in objects[0].providers:
 				newObj = ClientProviders()
 				newObj = objects[0]
-				newObj.providers.append("tripadvisor")
+				newObj.providers.append("booking")
 				newObj.save()
 
-def minitask():
-		obj2=TripAdvisorQ()
-		obj2.base_url=i[2]
-		obj2.survey_id=survey_id
-		obj2.parent_id=self.c
-		obj2.unique_identifier=survey_id+"tripadvisor"
-		obj2.save()
+
 if __name__ == '__main__':
 	# main()
-	CustomTask(file_name,"BLNVGrxZlwBYZ81veV3",2).run_csv()
+	CustomTask(file_name,"XomY5oG3kvr5grpyoBV",2).run_csv()
