c51ef6a0f196610b74856aa65a666ac25225cf54
Lokesh <lokeshmeher34@gmail.com>
2016-09-05 17:23:24 +0530

    Removed temporary file temp.py used for debugging

--- jupiter/api/__init__.py
+++ jupiter/api/__init__.py
@@ -6,14 +6,14 @@
 
 This module exists with following simple goals:
 - Manage tasks with a set of credentials into the periodic task runners.
-  - Credentials are the Zomato/Tripadvisor/etc., IDs and other information
-    that are sufficient to perform the aggregation tasks and any other stuff.
+    - Credentials are the Zomato/Tripadvisor/etc., IDs and other information
+      that are sufficient to perform the aggregation tasks and any other stuff.
 - Management provides the following interfaces via REST Endpoints:
-  - Add a credential
-  - Get the status
-  - Revoke/Delete the task
+    - Add a credential
+    - Get the status
+    - Revoke/Delete the task
 - The API also provides an interface to subscribe to server webhooks which
   are sent to the requesting server when the tasks are completed.
-  - Add a webhook subscription
-  - Remove the subscription
+    - Add a webhook subscription
+    - Remove the subscription
 """
